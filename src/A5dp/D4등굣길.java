2bc78333a3459f998b5fa9e3bcd7d640a47ba34c
wisdom <wisdom0405@kakao.com>
2024-06-26 21:51:31 +0900

    Update D4등굣길.java

--- src/A5dp/D4등굣길.java
+++ src/A5dp/D4등굣길.java
@@ -4,53 +4,82 @@ import java.util.Arrays;
 
 public class D4등굣길 {
     public static void main(String[] args) {
-        int m = 4;
-        int n = 3;
-        int[][] road = new int[m][n]; // 0부터 시작 (집: 0,0) (학교: 3,2)
-        int[][] puddles = {{2,2}};
+        int m = 4; // 열
+        int n = 3; // 행
 
-        for(int i=0; i<m; i++) {
-            Arrays.fill(road[i], -1); // 모든길 -1값으로 초기화
-        }
+        int[][] road = new int[n][m]; // 0부터 시작 (집: 0,0) (학교: 3,2)
+        int[][] puddles = {{2, 2}};
+        int mod = 1000000007;
 
-        // 물웅덩이가 있는 곳 표시하기
-        for(int i = 0; i < puddles.length; i++){
-            int x = puddles[i][0] - 1; // 물웅덩이 x좌표 index
-            int y = puddles[i][1] - 1; // 물웅덩이 y좌표 index
-            road[x][y] = 0; // 물웅덩이가 있는 곳은 -1로 표시
-        }
-        // 오른쪽, 아래로만 움직일 수 있음
-        // 집에서 오른쪽 끝까지 갈 수 있는 방법 : 1가지밖에 없음
-        for(int i = 0; i < m; i++){
-            road[i][0] = 1;
-        }
-        // 집에서 아래쪽 끝까지 갈 수 있는 방법 : 1가지밖에 없음
-        for(int i = 0; i < n; i++){
-            road[0][i] = 1;
+        // 물웅덩이가 있는 곳 -1로 표시하기
+        for (int[] puddle : puddles) {
+            road[puddle[1] - 1][puddle[0] - 1] = -1;
         }
 
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                if(road[i][j] == 0) break; // 현재위치가 물웅덩이면 pass
-                if((i-1) > 0 && (j-1) > 0){ // 왼쪽, 위가 존재한다면
-                    road[i][j] = road[i-1][j] + road[i][j-1];
-                }else if((i-1) > 0 && (j-1) < 0){
-                    road[i][j] = road[i-1][j];
-                }else if((i-1) < 0 && (j-1) > 0){
-                    road[i][j] = road[i][j-1];
+        // 시작점 초기화
+        road[0][0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // 웅덩이일 경우 0으로 바꿔주고 건너뜀
+                if (road[i][j] == -1) {
+                    road[i][j] = 0;
+                    continue;
                 }
+
+                // 위에서 오는 경로 추가
+                if (i > 0) {
+                    road[i][j] += road[i - 1][j] % mod;
+                }
+
+                // 왼쪽에서 오는 경로 추가
+                if (j > 0) {
+                    road[i][j] += road[i][j - 1] % mod;
+                }
+
+                road[i][j] %= mod;
             }
         }
 
-
         System.out.println(Arrays.deepToString(road));
-        System.out.println(road[m-1][n-1]);
-
-    }
-    class Solution {
-        public int solution(int m, int n, int[][] puddles) {
-            int answer = 0;
-            return answer;
-        }
+        System.out.println(road[n - 1][m - 1] % mod);
     }
 }
+
+    class Solution {
+        public int solution(int m, int n, int[][] puddles) {
+            int[][] road = new int[n][m]; // 0부터 시작 (집: 0,0) (학교: 3,2)
+            int mod = 1000000007;
+            // 물웅덩이가 있는 곳 -1로 표시하기
+            for (int[] puddle : puddles) {
+                road[puddle[1] - 1][puddle[0] - 1] = -1;
+            }
+
+            // 시작점 초기화
+            road[0][0] = 1;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    // 웅덩이일 경우 0으로 바꿔주고 건너뜀
+                    if (road[i][j] == -1) {
+                        road[i][j] = 0;
+                        continue;
+                    }
+
+                    // 위에서 오는 경로 추가
+                    if (i > 0) {
+                        road[i][j] += road[i - 1][j] % mod;
+                    }
+
+                    // 왼쪽에서 오는 경로 추가
+                    if (j > 0) {
+                        road[i][j] += road[i][j - 1] % mod;
+                    }
+
+                    road[i][j] %= mod;
+                }
+            }
+            return road[n - 1][m - 1] % mod;
+        }
+    }
+
